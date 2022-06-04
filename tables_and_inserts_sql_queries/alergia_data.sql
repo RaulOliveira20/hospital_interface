78f40db0a0bf9f3205908257f88466ac21ea75bd
RaulOliveira20 <36230040+RaulOliveira20@users.noreply.github.com>
2022-06-05 00:25:18 +0100

    Create alergia_data.sql

---
+++ tables_and_inserts_sql_queries/alergia_data.sql
@@ -0,0 +1,31 @@
+-- alergies: (Egg, Fish, Fruit, Garlic, Hot peppers, Oats, Corn, Milk, Peanut, Poultry Meat, Red Meat,
+-- Rice, Shellfish, Sesame, Soy, Wheat, Pollen, Cat, Dog, Mold, Insect sting, Perfume, Water,
+-- House dust mite, Fungicide)
+
+-- insert 25 rows
+
+INSERT INTO Alergia VALUES (1,'Egg');
+INSERT INTO Alergia VALUES (2,'Fish');
+INSERT INTO Alergia VALUES (3,'Fruit');
+INSERT INTO Alergia VALUES (4,'Garlic');
+INSERT INTO Alergia VALUES (5,'Hot peppers');
+INSERT INTO Alergia VALUES (6,'Oats');
+INSERT INTO Alergia VALUES (7,'Corn');
+INSERT INTO Alergia VALUES (8,'Milk');
+INSERT INTO Alergia VALUES (9,'Peanut');
+INSERT INTO Alergia VALUES (10,'Poultry Meat');
+INSERT INTO Alergia VALUES (11,'Red Meat');
+INSERT INTO Alergia VALUES (12,'Rice');
+INSERT INTO Alergia VALUES (13,'Shellfish');
+INSERT INTO Alergia VALUES (14,'Sesame');
+INSERT INTO Alergia VALUES (15,'Soy');
+INSERT INTO Alergia VALUES (16,'Wheat');
+INSERT INTO Alergia VALUES (17,'Pollen');
+INSERT INTO Alergia VALUES (18,'Cat');
+INSERT INTO Alergia VALUES (19,'Dog');
+INSERT INTO Alergia VALUES (20,'Mold');
+INSERT INTO Alergia VALUES (21,'Insect sting');
+INSERT INTO Alergia VALUES (22,'Perfume');
+INSERT INTO Alergia VALUES (23,'Water');
+INSERT INTO Alergia VALUES (24,'House dust mite');
+INSERT INTO Alergia VALUES (25,'Fungicide');
